7b1c65980d1d99aaa678bf7cef2ea62cca41cae0
oyj135 <107733358+oyj135@users.noreply.github.com>
2024-08-17 12:47:02 +0800

    Delete src directory

--- src/app.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineAppConfig({
-  pages: ['pages/doQuestion/index','pages/index/index'],
-  window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'MBTI性格测试',
-    navigationBarTextStyle: 'black'
-  }
-})
