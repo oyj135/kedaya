0939fee39f7fb3d0e07afd6108155de3436bca69
yangjian <oyjyyds@outlook.com>
2024-08-19 12:45:08 +0800

    主页和答题页面开发

---
+++ mbti-test-mini/src/app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  pages: [
+    'pages/doQuestion/index','pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'Jason_Yang MBTI 性格测试',
+    navigationBarTextStyle: 'black'
+  }
+})
