7f9614ac079779be3dcbc57f9f38b4160a3884f0
yangjian <oyjyyds@outlook.com>
2024-09-10 13:21:41 +0800

    路由设置

--- kedaya-frontend/src/components/GlobalHeader.vue
+++ kedaya-frontend/src/components/GlobalHeader.vue
@@ -1,29 +1,75 @@
 <template>
-  <div class="globalHeader">
-    <a-menu mode="horizontal" :default-selected-keys="['1']">
-      <a-menu-item
-        key="0"
-        :style="{ padding: 0, marginRight: '38px' }"
-        disabled
+  <a-row id="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :default-selected-keys="selectedkeys"
+        @menu-item-click="doMenuClick"
       >
-        <div
-          :style="{
-            width: '80px',
-            height: '30px',
-            borderRadius: '2px',
-            background: 'var(--color-fill-3)',
-            cursor: 'text',
-          }"
-        />
-      </a-menu-item>
-      <a-menu-item key="1">Home</a-menu-item>
-      <a-menu-item key="2">Solution</a-menu-item>
-      <a-menu-item key="3">Cloud Service</a-menu-item>
-      <a-menu-item key="4">Cooperation</a-menu-item>
-    </a-menu>
-  </div>
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="titleBar">
+            <img class="logo" src="../assets/logo.png" />
+            <div class="title">可达鸭</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        <a-button type="primary" href="/user/login">login</a-button>
+      </div>
+    </a-col>
+  </a-row>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-<style scoped></style>
+const router = useRouter();
+//当前选中的菜单项
+const selectedkeys = ref(["/"]);
+//路由更新时，自动更新选择的菜单项
+router.afterEach((to, from, failure) => {
+  selectedkeys.value = [to.path];
+});
+
+//展示在菜单栏的路由数组
+const visibleRoutes = routes.filter((item) => {
+  if (item.meta?.hideInMenu) {
+    return false;
+  }
+  return true;
+});
+
+//点击菜单跳转到对应页面
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<style scoped>
+.titleBar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
--- kedaya-frontend/src/views/HomeView.vue
+++ kedaya-frontend/src/views/HomeView.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 const a = 1;
 </script>
 
