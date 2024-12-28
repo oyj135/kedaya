463b9d75d495700db72aadf510f3d184c4498950
OuYJ <oyjyyds@outlook.com>
2024-12-28 16:02:13 +0800

    评分策略执行器

--- kedaya-backend/src/main/java/com/yj/kedaya/scoring/ScoringStrategyExecutor.java
+++ kedaya-backend/src/main/java/com/yj/kedaya/scoring/ScoringStrategyExecutor.java
@@ -1,5 +1,13 @@
 package com.yj.kedaya.scoring;
 
+import com.yj.kedaya.common.ErrorCode;
+import com.yj.kedaya.exception.BusinessException;
+import com.yj.kedaya.model.entity.App;
+import com.yj.kedaya.model.entity.UserAnswer;
+
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * 评分策略执行器
  *
@@ -8,4 +16,34 @@ package com.yj.kedaya.scoring;
  * @Date 2024/12/27 22:19
  */
 public class ScoringStrategyExecutor {
+
+    //策略列表
+    @Resource
+    private List<ScoringStrategy> scoringStrategyList;
+
+    /**
+     * 评分
+     *
+     * @param choiceList
+     * @param app
+     * @return
+     * @throws Exception
+     */
+    public UserAnswer doScore(List<String> choiceList, App app) throws Exception {
+        Integer appType = app.getAppType();
+        Integer appScoringStrategy = app.getScoringStrategy();
+        if (appType == null || appScoringStrategy == null) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "应用配置有误，未找到匹配的策略");
+        }
+        // 根据注解获取策略
+        for (ScoringStrategy strategy : scoringStrategyList) {
+            if (strategy.getClass().isAnnotationPresent(ScoringStrategyConfig.class)) {
+                ScoringStrategyConfig scoringStrategyConfig = strategy.getClass().getAnnotation(ScoringStrategyConfig.class);
+                if (scoringStrategyConfig.appType() == appType && scoringStrategyConfig.scoringStrategy() == appScoringStrategy) {
+                    return strategy.doScore(choiceList, app);
+                }
+            }
+        }
+        throw new BusinessException(ErrorCode.SYSTEM_ERROR, "应用配置有误，未找到匹配的策略");
+    }
 }
