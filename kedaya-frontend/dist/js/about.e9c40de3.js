99df83dda106335c04d39657d224196546cfd09b
OuYJ <oyjyyds@outlook.com>
2025-01-11 15:13:20 +0800

    update

---
+++ kedaya-frontend/dist/js/about.e9c40de3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkedaya_frontend"]=self["webpackChunkkedaya_frontend"]||[]).push([[594],{5609:function(n,a,e){e.r(a),e.d(a,{default:function(){return i}});var t=e(6768);const u={class:"about"},r=(0,t.Lk)("h1",null,"This is an about page",-1),s=[r];function c(n,a){return(0,t.uX)(),(0,t.CE)("div",u,s)}var o=e(1241);const d={},f=(0,o.A)(d,[["render",c]]);var i=f}}]);
+//# sourceMappingURL=about.e9c40de3.js.map
