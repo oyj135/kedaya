73f8a1e7c4fe18a9f5289fa27dcb98826f858588
OuYJ <oyjyyds@outlook.com>
2025-01-17 17:31:39 +0800

    增加redission和分库分表

---
+++ kedaya-backend/src/main/java/com/yj/kedaya/config/RedissonConfig.java
@@ -0,0 +1,9 @@
+package com.yj.kedaya.config;
+
+/**
+ * @Author  阳健
+ * @Package com.yj.kedaya.config
+ * @Date  2025/1/17 16:41
+*/
+public class RedissonConfig {
+}
---
+++ kedaya-backend/src/test/java/com/yj/kedaya/UserAnswerShardingTest.java
@@ -0,0 +1,9 @@
+package com.yj.kedaya;
+
+/**
+ * @Author  阳健
+ * @Package com.yj.kedaya
+ * @Date  2025/1/17 17:08
+*/
+public class UserAnswerShardingTest {
+}
